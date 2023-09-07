a47f7eac4a5fed6e4dbac0a616ae1552aa239d2b
Daniil Krishtofik <d.krishtofik@itransition.com>
2023-09-07 09:48:41 +0300

    Updates

--- src/components/TodoHeader.tsx
+++
@@ -1,7 +0,0 @@
-export default function TodoHeader() {
-  return (
-    <div className='row text-center'>
-      <div className='col todo-header'>todos</div>
-    </div>
-  );
-}
--- src/tests/setup.ts
+++
@@ -1,5 +0,0 @@
-import Enzyme from "enzyme";
-import Adapter from "@cfaester/enzyme-adapter-react-18";
-import "@testing-library/jest-dom/extend-expect";
-
-Enzyme.configure({ adapter: new Adapter() });
